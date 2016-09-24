cf585089e794bfdae5e73a6334e01690a2f40d85
Fabio <fabioemidiosouza@gmail.com>
2016-09-24 01:51:23 -0300

    feito atualização que o prof pediu

--- src/br/com/marketedelivery/camada/dados/IProdutoDao.java
+++
@@ -1,8 +0,0 @@
-package br.com.marketedelivery.camada.dados;
-
-import br.com.marketedelivery.camada.classesBasicas.Produto;
-
-public interface IProdutoDao extends IDAOGenerico<Produto>
-{
-	public Produto buscarProdutoPorCodigo(String codigo);
-}
