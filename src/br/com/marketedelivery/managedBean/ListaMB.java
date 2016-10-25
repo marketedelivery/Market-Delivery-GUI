c7652eb122e3da2171f28521537e8a5a447c1136
JoseClebson <j.clebson@gmail.com>
2016-10-25 20:57:11 -0300

    listando as listas prontas

---
+++ src/br/com/marketedelivery/managedBean/ListaMB.java
@@ -0,0 +1,63 @@
+package br.com.marketedelivery.managedBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.marketedelivery.Fachada.Fachada;
+import br.com.marketedelivery.IFachada.IFachada;
+import br.com.marketedelivery.classesBasicas.ListaDeCompras;
+
+@ViewScoped
+@ManagedBean
+public class ListaMB 
+{	
+	private ListaDeCompras lista;
+	private List<ListaDeCompras> listarTodos;
+	private List<ListaDeCompras> listaFiltrados;
+	private IFachada fachada;		
+	
+	public ListaDeCompras getLista() {
+		return lista;
+	}
+
+	public void setLista(ListaDeCompras lista) {
+		this.lista = lista;
+	}
+
+	public List<ListaDeCompras> getListaFiltrados() {
+		return listaFiltrados;
+	}
+
+	public void setListaFiltrados(List<ListaDeCompras> listaFiltrados) {
+		this.listaFiltrados = listaFiltrados;
+	}
+
+	public void setListarTodos(List<ListaDeCompras> listarTodos)
+	{
+		this.listarTodos = listarTodos;
+	}
+
+	public List<ListaDeCompras> getListarTodos()
+	{
+		return listarTodasLista();
+	}
+	
+	public IFachada getFachada() 
+	{
+		if(fachada == null)
+		{
+			return fachada = new Fachada();
+		}
+		return fachada;
+	}
+
+	public List<ListaDeCompras> listarTodasLista()
+	{
+		listarTodos = getFachada().ListarTodasAsListas();
+		return listarTodos;
+		
+	}
+}
