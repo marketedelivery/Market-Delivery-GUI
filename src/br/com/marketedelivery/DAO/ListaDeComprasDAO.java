d6e0d213b0d80522a61ecbd718f5dd73ab344bbc
JoseClebson <j.clebson@gmail.com>
2016-10-26 21:03:50 -0300

    adicionado mais uma validação no validar cpf

--- src/br/com/marketedelivery/DAO/ListaDeComprasDAO.java
+++ src/br/com/marketedelivery/DAO/ListaDeComprasDAO.java
@@ -1,10 +1,13 @@
 package br.com.marketedelivery.DAO;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import br.com.marketedelivery.IDAO.IListaDeComprasDAO;
 import br.com.marketedelivery.classesBasicas.ListaDeCompras;
+import br.com.marketedelivery.classesBasicas.Usuario;
 
 
 
@@ -26,6 +29,22 @@ public class ListaDeComprasDAO extends DAOGenerico<ListaDeCompras> implements IL
         ListaDeCompras resultado;
         try {
             resultado = retorno.getSingleResult();
+            return resultado;
+
+        } catch (Exception e) {
+            return null;
+
+        }
+    }
+    
+    public List<ListaDeCompras> buscaListaPorUsuario(Usuario usuario)
+    {
+        String consulta = "SELECT lista FROM ListaDeCompras lista WHERE lista.usuario.codigo = :N";
+        TypedQuery<ListaDeCompras> retorno = getEntityManager().createQuery(consulta, ListaDeCompras.class);
+        retorno.setParameter("N", usuario.getCodigo());
+        List<ListaDeCompras> resultado;
+        try {
+            resultado = retorno.getResultList();
             return resultado;
 
         } catch (Exception e) {
--- src/br/com/marketedelivery/util/ValidarCpf.java
+++ src/br/com/marketedelivery/util/ValidarCpf.java
@@ -5,11 +5,16 @@ public class ValidarCpf
 	private String cpf; 
 	  public ValidarCpf() {
 	  }
-	  public boolean validarCpf(String cpf){  
-	      if ( cpf == null ){
-	          return false;
-	      }
-	      else{
+	  public boolean validarCpf(String cpf)
+	  {
+		  if (cpf.equals("00000000000") || cpf.equals("11111111111") ||
+			        cpf.equals("22222222222") || cpf.equals("33333333333") ||
+			        cpf.equals("44444444444") || cpf.equals("55555555555") ||
+			        cpf.equals("66666666666") || cpf.equals("77777777777") ||
+			        cpf.equals("88888888888") || cpf.equals("99999999999") ||
+			       (cpf.length() != 11) || cpf == null)
+			       return(false);
+		else {
 	              String cpfGerado = "";     
 	              this.cpf = cpf;           
 	               removerCaracteres();            
