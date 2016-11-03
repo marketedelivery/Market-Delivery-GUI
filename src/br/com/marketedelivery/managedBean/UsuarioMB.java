2e3c68202da8f687e42c516f4de5ad583a8f46c0
Junior <jfsjuniorjcs@gmail.com>
2016-11-03 00:54:30 -0300

    Modificado o layout do menu de vertical para horizontal. Retirado os codigos na exibição de pesquisa (produto,supermercado e cliente).

--- src/br/com/marketedelivery/managedBean/UsuarioMB.java
+++ src/br/com/marketedelivery/managedBean/UsuarioMB.java
@@ -24,8 +24,7 @@ import br.com.marketedelivery.util.ValidarEmail;
 @RequestScoped
 @ViewScoped
 @ManagedBean(name = "usuarioMB")
-public class UsuarioMB implements Serializable
-{
+public class UsuarioMB implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Usuario usuario;
@@ -42,92 +41,77 @@ public class UsuarioMB implements Serializable
 
 	ValidarCpf validarCPF = new ValidarCpf();
 
-	public Endereco getEndereco()
-	{
-		if (endereco == null)
-		{
+	public Endereco getEndereco() {
+		if (endereco == null) {
 			endereco = new Endereco();
 		}
 		return endereco;
 	}
 
-	public void setEndereco(Endereco endereco)
-	{
+	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
 
-	public void setUsuario(Usuario usuario)
-	{
+	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
 
-	public Usuario getUsuario()
-	{
-		if (usuario == null)
-		{
+	public Usuario getUsuario() {
+		if (usuario == null) {
 			usuario = new Usuario();
 		}
 		return usuario;
 	}
 
-	public IFachada getFachada()
-	{
-		if (fachada == null)
-		{
+	public IFachada getFachada() {
+		if (fachada == null) {
 			fachada = new Fachada();
 		}
 		return fachada;
 	}
 
-	public void setFachada(IFachada fachada)
-	{
+	public void setFachada(IFachada fachada) {
 		this.fachada = fachada;
 	}
 
-	public List<Usuario> getListar()
-	{
+	public List<Usuario> getListar() {
 		return listar;
 	}
 
-	public void setListar(List<Usuario> listar)
-	{
+	public void setListar(List<Usuario> listar) {
 		this.listar = listar;
 	}
 
-	public List<Usuario> getListaUsuarios()
-	{
+	public List<Usuario> getListaUsuarios() {
 		return listaUsuarios;
 	}
 
-	public void setListaUsuarios(List<Usuario> listaUsuarios)
-	{
+	public void setListaUsuarios(List<Usuario> listaUsuarios) {
 		this.listaUsuarios = listaUsuarios;
 	}
 
-	public List<Usuario> getListaUsuariosFiltrados()
-	{
+	public List<Usuario> getListaUsuariosFiltrados() {
 		return listaUsuariosFiltrados;
 	}
 
-	public void setListaUsuariosFiltrados(List<Usuario> listaUsuariosFiltrados)
-	{
+	public void setListaUsuariosFiltrados(List<Usuario> listaUsuariosFiltrados) {
 		this.listaUsuariosFiltrados = listaUsuariosFiltrados;
 	}
 
-	public Estado[] getUFs()
-	{
+	public Estado[] getUFs() {
 		return Estado.values();
 	}
 
-	public void salvar()
-	{
+	public void salvar() {
 		usuario.setEndereco(endereco);
 		fachada = getFachada();
 		Usuario user = fachada.listarPorCPF(usuario);
-		try
-		{
+		try {
 			if (validarCPF.validarCpf(usuario.getCpf()) != true && ValidarEmail.emailValido(usuario.getEmail()) != true
 					&& user == null)
+			// if (validarCPF.validarCpf(usuario.getCpf()) != true && user ==
+			// null)
+
 			{
 				fachada.cadastrarUsuario(usuario);
 				// FacesContext.getCurrentInstance().getExternalContext().redirect(/*
@@ -144,81 +128,63 @@ public class UsuarioMB implements Serializable
 				// ec.redirect(ec.getRequestContextPath()+"/pages/protected/minhasListas.xhtml?faces-redirect=true");
 				// FacesContext.getCurrentInstance().getExternalContext()
 				// .redirect("/pages/public/login.xhtml?faces-redirect=true");
-			} else if (user != null)
-			{
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Esse cpf já possui cadastro!"));
+			} else if (user != null) {
+				FacesContext.getCurrentInstance().addMessage(null,
+						new FacesMessage("Esse cpf já consta em nossos registros!"));
 				return;
-			} else if (validarCPF.validarCpf(usuario.getCpf()) == false)
-			{
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Esse cpf Invalido!"));
+			} else if (validarCPF.validarCpf(usuario.getCpf()) == false) {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Esse cpf é inválido!"));
 				return;
-			} else if (ValidarEmail.emailValido(usuario.getEmail()) == false)
-			{
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Esse e-mail Invalido!"));
+			} else if (ValidarEmail.emailValido(usuario.getEmail()) == false) {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Esse e-mail é inválido!"));
 				return;
 			}
 			usuario = new Usuario();
 			endereco = new Endereco();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage("Erro ao cadastrar, tente novamente mais tarde"));
 			System.out.println(e.getMessage());
 		}
 	}
 
-	public void carregarPesquisa()
-	{
-		try
-		{
+	public void carregarPesquisa() {
+		try {
 			// fachada.ListarTodosUsuarios();
 			// listaUsuarios = fachada.ListarTodosUsuarios();
 			IFachada fachada = getFachada();
 			listaUsuarios = fachada.listarTodosUsuarios();
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			FacesUtil.adicionarMsgErro("Erro ao tentar listar os usuarios" + ex.getMessage());
 		}
 	}
 
 	@PostConstruct
-	public void carregarCadastro()
-	{
-		try
-		{
+	public void carregarCadastro() {
+		try {
 			String valor = FacesUtil.getParam("clicod");
 			// int codigo_pessoa = 0;
-			if (valor != null)
-			{
+			if (valor != null) {
 				// int codigo = Integer.parseInt(valor);
 				usuario = fachada.pesquisarPorCodigo(usuario);
 			}
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			FacesUtil.adicionarMsgErro("Erro ao tentar listar os usuarios" + ex.getMessage());
 		}
 	}
 
-	public void alterar(Usuario usuario)
-	{
+	public void alterar(Usuario usuario) {
 		{
 			fachada = getFachada();
 			Usuario u = fachada.listarPorCPF(usuario);
-			try
-			{
-				if (u != null)
-				{
+			try {
+				if (u != null) {
 					u = usuario;
 					fachada.atualizarUsuario(u);
 					FacesContext.getCurrentInstance().addMessage(null,
 							new FacesMessage("Cadastro Alterado com Sucesso"));
 				}
-			}
-			catch (Exception e)
-			{
+			} catch (Exception e) {
 				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Erro ao Atualizar"));
 				System.out.println(e.getMessage());
 			}
